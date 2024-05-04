03f0cef5ddf37a87cd0a4dfa116877750540a7b8
UltimaVanguard <ultimavanguard@gmail.com>
2024-05-04 15:27:29 -0500

    fixed delete routes for all models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -66,7 +66,11 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const categoryData = await Category.destroy(req.params.id);
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id, 
+      },
+    });
 
     if (!categoryData) {
       res.status(404).json({ message: 'Category not found!'});
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -116,7 +116,11 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const productData = await Product.destroy(req.params.id);
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id, 
+      },
+    });
 
     if (!productData) {
       res.status(404).json({ message: 'Category not found!'});
