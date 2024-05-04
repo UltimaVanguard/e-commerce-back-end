c21f79bee54b40c264462f084f1ab0c2357b1954
UltimaVanguard <ultimavanguard@gmail.com>
2024-05-04 12:42:29 -0500

    added get, get by id, and delete routes for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ all: true }]
+    });
+    res.status(200).json(productData);
+  } catch(err) {
+    res.status(500).json({ message: err });
+  };
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, 
+      { include: [{ all: true }] }
+    );
+
+    if (!productData) {
+      res.status(404).json({ message: 'Product not found!'});
+    };
+
+    res.status(200).json(productData);
+  } catch(err) {
+    res.status(500).json({ message: err });
+  };
 });
 
 // create new product
@@ -92,8 +113,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy(req.params.id);
+
+    if (!productData) {
+      res.status(404).json({ message: 'Category not found!'});
+    };
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  };
 });
 
 module.exports = router;
